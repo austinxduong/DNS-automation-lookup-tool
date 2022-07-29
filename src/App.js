6ffe7ec5d6b70521e0069cd3b5343a0f1a0c220c
austinxduong <axduong2@gmail.com>
2022-07-28 23:02:39 -0700

    rick-roll that ish

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      DNS Automation Lookup 
         { /* play with three.js or SVG....?  hmm.. <img src={logo} className="App-logo" alt="logo" />*/}
       </header>
       <body>
       <p>
-          DNS Automation Lookup 
         </p>
         <input type="text" alt="domain_input_field" placeholder="Domain" />
           <select name="records" id="records-id">
