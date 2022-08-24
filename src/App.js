46730a11efb90e59e6b81a2dcef731e0e88d562d
austinxduong <axduong2@gmail.com>
2022-08-23 23:21:32 -0700

    domain input value is record in object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import DomainInput from './components/DomainInput';
 
 function App() {
 
-  const [domain, setDomain] = useState("")
-  console.log(domain)
+  // const [domain, setDomain] = useState("")
+const domainInputRef = useRef()
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(domainInputRef)
+}
+
 
   return (
     <div className="App">
@@ -14,7 +20,10 @@ function App() {
       DNS Automation Lookup 
         { /* play with three.js or SVG....?  hmm.. <img src={logo} className="App-logo" alt="logo" />*/}
       </header>
-      <DomainInput placeholder="Domain" setDomain={setDomain}/>
+        <form id="form" method="get" action="/action_page.php" onSubmit={handleSubmit}>
+          <DomainInput placeholder="Domain" refer={domainInputRef} />
+          <button> Search Records</button>
+        </form>
     </div>
   );
 }
--- src/components/DomainInput.js
+++ src/components/DomainInput.js
@@ -3,20 +3,18 @@ import './DomainInput.js';
 import './DomainInput.css';
 
 const DomainInput = (props) => {
+
   return (
-    <div>
-    <body>
-        <form id="form" method="get" action="/action_page.php">
-          <input type="text" alt="domain_input_field" placeholder={props.placeholder} onChange={e => props.setDomain(e.target.value)}required/>
+    <div className="domainInput">
+      <body>
+          <input type="text" alt="domain_input_field" placeholder={props.placeholder} ref={props.refer} required/>
             <select name="records" id="records-id">
             <option value="mx"> MX Record</option>
             <option value="mx"> TXT Record</option>
             <option value="mx"> CNAME Record</option>
             <input type="submit"></input>
             </select>
-          </form>
-      </body>
-      <button> Search Records</button>
+        </body>
     </div>
   )
 }
