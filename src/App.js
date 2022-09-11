7e64b9322aba35be4adacf1593221981151dc88d
austinxduong <axduong2@gmail.com>
2022-09-10 17:52:32 -0700

    reorganizing layered components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 // import logo from './logo.svg';
-import { useRef, useState } from 'react';
+// import { useRef, useState } from 'react';
 import './App.css';
-import DomainInput from './components/DomainInput';
+import Form from './components/Form'
 import Header from './components/Header';
+// import Footer from './components/Footer'
 
 function App() {
 
-const [domain, setDomain] = useState("")
-// const [record, setRecord] = useState(null);
-const domainInputRef = useRef()
+
 
 // function getRecordType() {
 //   switch(record) {
@@ -19,25 +18,15 @@ const domainInputRef = useRef()
 //   }
 // }
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(domainInputRef)
-}
-
-
   return (
     <div className="App">
       <Header />
-      <body>
-        <div class ="search"> {domain} </div>
-          <form id="form" method="get" action="/action_page.php" onSubmit={handleSubmit} onChange={e => setDomain(e.target.value)}>
-            <DomainInput placeholder="Domain" id="domain-input-placeholder" refer={domainInputRef} />
-            <button> Search Records</button>
-            {/* {getRecordType()} */}
-          </form>
-      </body>
+        <body>
+          <Form />
+        </body>
 
-      <footer> </footer>
+        <footer> 
+        </footer>
     </div>
   );
 }
