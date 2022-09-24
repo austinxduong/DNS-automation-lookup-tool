73a4fbec81d435282ef26e893ff67bae38ba2bdf
austinxduong <axduong2@gmail.com>
2022-09-23 23:26:59 -0700

    update update

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,10 +3,12 @@ import * as yup from "yup";
 export const initSchema = yup.object().shape({
     domain: yup.string().required("Please enter a valid domain"),
 })
+
+
 export const recordSchema = yup.object().shape({
     records: yup.string().required("Please select a record"),
     recordType: yup
     .string()
-    .oneOf(["MX, TXT, CNAME"])
+    .oneOf(["MX, TXT, CNAME", "SSL CERTIFICATE"])
     .required("required"),
 })
